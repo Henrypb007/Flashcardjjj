080a24bd38150cf0e39602a74a993c492300b7a1
Henrypb007 <henry.bahls.souza@escola.pr.gov.br>
2025-07-01 11:14:36 -0300

    Create p.js

---
+++ p.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'automotivo',
+    'Quando a Porshe foi fundada e por quem?',
+    'foi fundada em 1931 por Ferdinand Porshe em Stuttgart na alemanha'
+)
+
+criaCartao(
+    'automotivo',
+    'qual a origem do nome Porshe?',
+    'o nome vem do sobrenome do fundador'
+)
+
+criaCartao(
+    'Automotivo',
+    'qual a história?',
+    'inicialmente a empresa atuava como consultoria de engenharia fazendo parte da volkswagen'
+)
+
+criaCartao(
+    'automotivo',
+    'qual o primeiro carro?',
+    'O primeiro carro foi criado em 1948 um carro esportivo com carroceria de aluminio e motor traseiro inspirado em fusca o nome foi dado 356'
+)
